120c42d4bb2c54bd9e874d2fd9e2a5137f211d3d
Lpstahl <lpstahl.dev@gmail.com>
2022-07-07 15:27:09 -0300

    interate the game manager

---
+++ Ebac/Core/Singleton/Singleton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ebac.Core.Singleton
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance;
+
+    public Singleton()
+    {
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = GetComponent<T>();
+        else
+            Destroy(gameObject);
+    }
+}
