5662144a6b8b0396df743c7b658fb1a80582370e
Lpstahl <lpstahl.dev@gmail.com>
2022-08-01 12:44:43 -0300

    SO UI Int update

---
+++ Assets/Scripts/Utils/UI/SOUIIntUpdate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SOUIIntUpdate : MonoBehaviour
+{
+    public SOInt soInt;
+    public TextMeshProUGUI uiTExtValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiTExtValue.text = soInt.value.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uiTExtValue.text = soInt.value.ToString();
+    }
+}
