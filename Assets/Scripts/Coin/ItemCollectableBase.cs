5443ba22b0062fb6e765f45832922282ea8fcf5d
Lpstahl <lpstahl.dev@gmail.com>
2022-08-12 23:07:20 -0300

    update collectable coin animation (working)

--- Assets/Scripts/Coin/ItemCollectableBase.cs
+++ Assets/Scripts/Coin/ItemCollectableBase.cs
@@ -22,8 +22,15 @@ public class ItemCollectableBase : MonoBehaviour
     }
     protected virtual void Collect()
     {
+        if (particleSystem != null)
+        {
+            particleSystem.transform.SetParent(null);
+            particleSystem.Play();  
+            Destroy(particleSystem.gameObject, 2f);
+        }
         coin.gameObject.SetActive(false);
         OnCollect();
+
     }
     protected virtual void OnCollect() 
     {
