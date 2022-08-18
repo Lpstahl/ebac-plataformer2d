a5dfb43d0068e42e0b12c3b7b586f123ac905775
Lpstahl <lpstahl.dev@gmail.com>
2022-08-17 23:09:08 -0300

    update gunBase verify vfx shoot direction

--- Assets/Scripts/Gun/GunBase.cs
+++ Assets/Scripts/Gun/GunBase.cs
@@ -9,6 +9,8 @@ public class GunBase : MonoBehaviour
     public Transform positionToShoot;
     public float timeBetweenShoot = .3f;
     public Transform playerSideReference;
+    //public Vector3 direction;
+    //public float side = 1;
 
     private Coroutine _currentCorotine;
 
@@ -36,7 +38,9 @@ public class GunBase : MonoBehaviour
     private void PlayShotVfx()
     {
         VFXManager.instance.PlayVFXByType(VFXManager.VFXType.SHOOT, transform.position);
-        
+        //CODE para VFX acompnhar a direção do shoot (verificar)
+        //transform.Translate(direction * Time.deltaTime * side);
+
     }
 
     IEnumerator StartShoot()
